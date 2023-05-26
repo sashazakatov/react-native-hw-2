dfc808652a9bc4451d63e5d10710c3b99abd1259
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-05-26 20:32:07 +0300

    Add RegistrationScreen

---
+++ App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+// import background from './src/assets/images/background.jpg'
+
+import LoginScreen from './src/screens/LoginScreen';
+import PostsScreen from './src/screens/PostsScreen';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+
+import { useFonts } from 'expo-font';
+
+export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    'Roboto-Bold': require('./src/assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('./src/assets/fonts/Roboto-Medium.ttf'),
+    'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
+  })
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+  return (
+    <>
+      <RegistrationScreen />
+      <StatusBar style='auto' />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative' ,
+  },
+  image: {
+
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
---
+++ src/screens/RegistrationScreen.js
@@ -0,0 +1,104 @@
+import { Pressable, Text, TextInput, View, StyleSheet, ImageBackground } from 'react-native';
+
+const RegistrationScreen = () => {
+    return(
+        <ImageBackground 
+            source={require('../assets/images/background.jpg')}
+            resizeMode="cover" 
+            style={style.container}
+        >
+            <View style={style.form}>
+                <View style={style.photo}>
+                    {/* <Button title='add'/> */}
+                </View>
+                <Text style={style.title}>Реєстрація</Text>
+                <TextInput
+                    style={style.input}
+                    placeholder='Логін'
+                />
+                <TextInput
+                    style={style.input}
+                    placeholder='Адреса електронної пошти'
+                />
+                <TextInput
+                    style={[style.input, {marginBottom: 43}]}
+                    placeholder='Пароль'
+                />
+                <Pressable style={style.button}>
+                    <Text style={{color: '#FFFFFF', fontSize: 16, }}>Нажми меня</Text>
+                </Pressable>
+                <Text style={style.text}>Вже є акаунт? Увійти</Text>
+            </View>
+        </ImageBackground>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    form: {
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+
+        alignItems: 'center',
+
+        paddingTop: 92,
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    photo: {
+        position: 'absolute',
+        top: -50,
+        backgroundColor: '#e1e5ee',
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:32, 
+    },
+    title: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        marginBottom: 33,
+        fontSize: 30,
+    },
+    input: {
+        height: 40,
+        width: '100%',
+        backgroundColor: 'whitesmoke',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 16,
+        '&:focus': {
+            borderColor: '#FF6C00',
+        },
+    },
+    button: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 40,
+        width: '100%',
+        fontFamily: 'Roboto',
+        color: '#FFFFFF',
+        backgroundColor: "#FF6C00",
+        borderRadius: 100,
+        marginBottom: 16,
+    },
+    text:{
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+        fontSize: 16,
+        color: "#1B4371",
+        marginBottom: 40,
+    }
+});
+
+export default RegistrationScreen;
